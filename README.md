8761dbc58edf71de086a880958dbde39843cadfa
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-17 20:34:01 +0300

    Created initial application setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pokemon-pokedex-react
+# Pokemon Pokedex
+Coming soon...
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pikachu!</h1>
+    </div>
+  );
+}
+
+export default App;
