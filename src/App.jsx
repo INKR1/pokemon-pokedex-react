66e30807dc6778b845ddfa334a22b49e030c3d36
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-18 20:07:52 +0300

    loaded pokemons using useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Pokedex from './components/Pokedex';
 import Searchbar from "./components/Searchbar";
+import { getPokemons } from './data/api';
+
 
 function App() {
+
+  const [pokemons, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true);
+      const result = await getPokemons();
+      const data = await result.json();
+      setPokemon(data.results);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    console.log("loaded");
+    fetchPokemons()
+  }, []);
 
   return (
     <div className="App">
       <Navbar />
       <Searchbar />
-      <Pokedex />
+      <Pokedex pokemons={pokemons} loading={loading}/>
     </div>
   );
 }
