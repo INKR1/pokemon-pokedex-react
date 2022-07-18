2c1965c4c0dae817c9c40818ef383c958d27bdbb
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-18 19:46:38 +0300

    geting all pokemons fetching API

--- src/data/api.js
+++ src/data/api.js
@@ -7,3 +7,13 @@ export const searchForPokemon = async (pokemonName) => {
     console.log("error", error);
   }
 };
+
+export const getPokemons = async (limit = 50, ofset = 0) => {
+    try {
+      let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${ofset}`;
+      const response = await fetch(url);
+      return await response.json();
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
