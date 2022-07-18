cf0621cff062155c482ba5b2936438d21d13c354
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-18 10:40:24 +0300

    Created navbar component and added style

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar() {
+  const logoImg = "https://sg.portal-pokemon.com/img/common/logo.png";
+  return (
+    <nav>
+      <div>
+        <img src={logoImg} alt="Pokemon-logo" className="navbar-img" />
+      </div>
+    </nav>
+  );
+}
