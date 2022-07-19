e17d779d010da08916bfcc1b7915790dadbf2960
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-19 16:04:16 +0300

    added logic to pokedex component

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
-export default function Pokedex() {
+export default function Pokedex(props) {
+
+  const { pokemons, loading } = props;
   return (
     <div>
-        <div className="pokedex-header" >
-            <h1>Pokedex</h1>
-        </div>
-    </div>
-  )
+      <div className="pokedex-header">
+        <h1>Pokedex</h1>
+        <div>Pages</div>
+      </div>
+      {loading ? (
+        <div>Loading the pokemons...</div>
+      ): (
+        <div className="pokedex-grid">
+          {pokemons.map((pokemon, index) => (
+            <div className="pokemon-card" key={index}>
+              <div> # {pokemon.id} </div>
+              <div> Name:{pokemon.name} </div>
+              <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            </div>
+          ))}
+          </div>
+      )}
+      </div>
+  );
 }
