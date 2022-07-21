8a58c219b5068d792a6c89d6882cc13416342407
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-21 09:08:30 +0300

    added more fetched data to pokemon

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 
-export default function Pokemon({pokemon}) {
-
+export default function Pokemon({ pokemon }) {
   return (
     <div className="pokemon-card">
       <div className="pokemon-img-container">
-        <div># {pokemon.id}</div>
         <img
           alt={pokemon.name}
           src={pokemon.sprites.front_default}
           className="pokemon-image"
         />
-        <div>Name: {pokemon.name}</div>
+      </div>
+      <div className="card-body">
+        <div className="card-top">
+          <h3>{pokemon.name}</h3>
+          <div># {pokemon.id} </div>
+        </div>
+        <div className="card-bottom">
+          <div className="pokemon-type">
+            {pokemon.types.map((type, index) => {
+              return (
+              <div className="pokemon-type-item" key={index}>
+                {type.type.name}
+              </div>
+              );
+            })}
+          </div>
+          <button className="liked-pokemon-btn">I like it</button>
+        </div>
       </div>
     </div>
   );
