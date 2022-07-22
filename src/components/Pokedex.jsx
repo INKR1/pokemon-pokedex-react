c19ad7e8292c8d3c0e370e19c8ed46ff1c69e3e6
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-22 13:49:23 +0300

    Added pages logic to pokedex component

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import Pages from "./Pages";
 import Pokemon from "./Pokemon";
 
-export default function Pokedex({pokemons, loading}) {
-  console.log("all pokemons ", pokemons)
+export default function Pokedex({pokemons, loading, page, setPage, totalPages}) {
+
+  const clickedLeftHandler = () => {
+    if(page > 0) {
+        setPage(page-1)
+    }
+}
+const clickedRightHandler = () => {
+    if(page+1 !== totalPages){
+        setPage(page+1)
+    }
+}
   return (
     <div>
       <div className="pokedex-header">
         <h1>Pokedex</h1>
-        <div>Pages</div>
+        <Pages 
+          page={page +1}
+          totalPages={totalPages}
+          clickedRight={clickedRightHandler}
+          clickedLeft={clickedLeftHandler}
+        />
       </div>
       {loading ? (
         <div>Loading the pokemons...</div>
