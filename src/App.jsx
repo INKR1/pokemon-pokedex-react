5869d80e3ba623823066bf387441f930a3764b50
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-22 13:48:44 +0300

    added pages to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,27 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Pokedex from "./components/Pokedex";
 import Searchbar from "./components/Searchbar";
-import { getPokemons, getPokemonData } from "./data/api";
+import { searchForPokemon, getPokemons, getPokemonData } from "./data/api";
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const pokemonsPerPage = 30; 
 
   const fetchPokemons = async () => {
     try {
       setLoading(true);
-      const data = await getPokemons();
+      const data = await getPokemons(pokemonsPerPage, pokemonsPerPage * page);
       const promises = data.results.map(
         async (pokemon) => await getPokemonData(pokemon.url)
         );
       const results = await Promise.all(promises);
       setPokemons(results);
       setLoading(false);
+      setTotalPages(Math.ceil(data.count / pokemonsPerPage));
     } catch (error) {
       console.log(error);
     }
@@ -27,13 +32,20 @@ function App() {
   useEffect(() => {
     console.log("loaded");
     fetchPokemons();
-  }, []);
+  }, [page]);
+
 
   return (
     <div className="App">
       <Navbar />
       <Searchbar />
-      <Pokedex pokemons={pokemons} loading={loading} />
+      <Pokedex 
+        pokemons={pokemons} 
+        loading={loading} 
+        page={page}
+        setPage={setPage}
+        totalPages={totalPages}
+      />
     </div>
   );
 }
