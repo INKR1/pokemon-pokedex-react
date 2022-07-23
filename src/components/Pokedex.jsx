af0e06fdf1cdc1358b7932555df811f7d6f26029
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-23 11:29:37 +0300

    Added loader

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -2,41 +2,46 @@ import React from "react";
 import Pages from "./Pages";
 import Pokemon from "./Pokemon";
 
-export default function Pokedex({pokemons, loading, page, setPage, totalPages}) {
-
+export default function Pokedex({
+  pokemons,
+  loading,
+  page,
+  setPage,
+  totalPages,
+}) {
   const clickedLeftHandler = () => {
-    if(page > 0) {
-        setPage(page-1)
+    if (page > 0) {
+      setPage(page - 1);
     }
-}
-const clickedRightHandler = () => {
-    if(page+1 !== totalPages){
-        setPage(page+1)
+  };
+  const clickedRightHandler = () => {
+    if (page + 1 !== totalPages) {
+      setPage(page + 1);
     }
-}
+  };
   return (
     <div>
       <div className="pokedex-header">
         <h1>Pokedex</h1>
-        <Pages 
-          page={page +1}
+        <Pages
+          page={page + 1}
           totalPages={totalPages}
-          clickedRight={clickedRightHandler}
           clickedLeft={clickedLeftHandler}
+          clickedRight={clickedRightHandler}
         />
       </div>
       {loading ? (
-        <div>Loading the pokemons...</div>
+        <article>
+          <div className="pokemon"></div>
+      </article>
       ) : (
         <div className="pokedex-grid">
           {pokemons &&
             pokemons.map((pokemon, index) => {
-              return (
-                <Pokemon key={index} pokemon={pokemon} />
-              );
+              return <Pokemon key={index} pokemon={pokemon} />;
             })}
         </div>
       )}
     </div>
   );
-};
+}
