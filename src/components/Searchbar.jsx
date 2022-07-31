a3c96b6d6303fe82ef6485e6ebdf95025381d5b6
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-31 16:55:30 +0300

    fixed searchbar style

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import { searchForPokemon } from "../data/api.js";
 
 export default function Searchbar(props) {
-
   const [search, setSearch] = useState("dito");
   const [pokemon, setPokemon] = useState();
   const [pokemonId, setPokemonId] = useState();
@@ -20,36 +19,43 @@ export default function Searchbar(props) {
 
   const onNameSearchHandler = async (pokemonName) => {
     const resultName = await searchForPokemon(pokemonName);
-    setPokemon(resultName)
-  }
+    setPokemon(resultName);
+  };
 
   const onIdSearchHandler = async (pokemonNumber) => {
     const resultNumber = await searchForPokemon(pokemonNumber);
-    setPokemonId(resultNumber)
-  }
+    setPokemonId(resultNumber);
+  };
 
   return (
-    <div className="searchbar-container">
-      <div className="searchbar">
-        <input
-          type="text"
-          placeholder="Search for a Pokemon"
-          onChange={onChangeHandler}
-        />
-      </div>
-      <div className="searchbar-button">
-        <button onClick={buttonClickHandler}>Search</button>
-      </div>
-      {pokemon || pokemonId ? (
-        <div className="pokemon-container">
-          <div> Name: {pokemon.name} </div>
-          <div> Weight: {pokemon.weight} </div>
-          <div> # {pokemon.id}</div>
-          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+    <div className="searchbar-wrapper">
+      <div className="searchbar-left">
+        <div className="searchbar">
+          <input
+            type="text"
+            placeholder="Search for a Pokemon"
+            onChange={onChangeHandler}
+          />
         </div>
-      ) : (
-            <h1> :)</h1>
-      ) }
+        <div className="searchbar-button">
+          <button onClick={buttonClickHandler}>Search</button>
+        </div>
+      </div>
+      <div className="searchbar-right">
+        {pokemon || pokemonId ? (
+          <div className="pokemon-container">
+            <div>
+              {" "}
+              Name: <span>{pokemon.name}</span>
+            </div>
+            <div> Weight: {pokemon.weight} </div>
+            <div> # {pokemon.id}</div>
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          </div>
+        ) : (
+          <h1> :)</h1>
+        )}
+      </div>
     </div>
   );
 }
