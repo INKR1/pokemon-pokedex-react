c4f960d149e1bdde94a12867aa184866b94e6062
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-08-24 15:58:28 +0300

    Wrapping app in context

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { FavCtxProvider } from './contexts/favPokemonContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <App />
+    // <App />
   // </React.StrictMode>
+  <FavCtxProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </FavCtxProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
