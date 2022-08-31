25a923a7156fc058c1fd1378ac77ac45db1e5b35
Ineta Kreiviene <ineta.kreiviene@icloud.com>
2022-08-31 10:42:26 +0300

    Moved pokedex component

---
+++ src/pages/Pokedex.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Pages from "../components/Pages";
+import Pokemon from "../components/Pokemon";
+
+export default function Pokedex({
+  pokemons,
+  loading,
+  page,
+  setPage,
+  totalPages,
+  likeButtonPressed,
+  likes
+}) {
+  const clickedLeftHandler = () => {
+    if (page > 0) {
+      setPage(page - 1);
+    }
+  };
+  const clickedRightHandler = () => {
+    if (page + 1 !== totalPages) {
+      setPage(page + 1);
+    }
+  };
+  return (
+    <div>
+      <div className="pokedex-header">
+        <h1>Pokedex</h1>
+        <Pages
+          page={page + 1}
+          totalPages={totalPages}
+          clickedLeft={clickedLeftHandler}
+          clickedRight={clickedRightHandler}
+        />
+      </div>
+      {loading ? (
+        <article>
+          <div className="pokemon"></div>
+      </article>
+      ) : (
+        <div className="pokedex-grid">
+          {pokemons &&
+            pokemons.map((p) => (
+                <Pokemon 
+                key={p.id} 
+                pokemon={p} 
+                like={likes.has(p.id)}
+                likeButtonPressed={likeButtonPressed}
+                />
+            ))}
+        </div>
+      )}
+    </div>
+  );
+}
