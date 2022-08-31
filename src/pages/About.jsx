1d3c730414d0321cf5d7fea1615a39a4beb2c0b5
Ineta Kreiviene <ineta.kreiviene@icloud.com>
2022-08-31 15:44:16 +0300

    Created about component

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='text-box'>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum quia ullam architecto minima ducimus debitis magnam harum nisi aliquam autem veniam, saepe quaerat! Quas officia quaerat eum deserunt esse deleniti sint ipsa cum, non quisquam itaque eos soluta numquam, dolore asperiores ipsam, accusamus odit? Incidunt hic voluptatem numquam eligendi sint.
+      </p>
+    </div>
+  )
+}
