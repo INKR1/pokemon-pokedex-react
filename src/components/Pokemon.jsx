48d22f2922c5e754a6d9735ffa71dbef686ec143
Ineta Kreiviene <ineta.kreiviene@icloud.com>
2022-09-01 11:14:56 +0300

    Used context in pokemon component

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,36 +1,61 @@
-export default function Pokemon({ pokemon, like, likeButtonPressed }) {
+import { useContext } from "react";
+
+import Card from "../components/ui/Card"
+import FavoritesContext from "../contexts/favPokemonContext";
+
+export default function Pokemon({ pokemon }) {
+  
+  const favCtx = useContext(FavoritesContext);
+
+  const pokemonIsFav = favCtx.pokemonIsFavorite(pokemon.id);
+
+  function toggleFavStatus() {
+    if (pokemonIsFav) {
+      favCtx.removeFromFavorites(pokemon.id);
+    } else {
+      favCtx.addToFavorite({
+        id: pokemon.id,
+        name: pokemon.name,
+        type: pokemon.type,
+        img: pokemon.sprites.front_default
+      });
+    }
+  }
+  
   return (
     <div className="pokemon-card">
-      <div className="pokemon-img-container">
-        <img
-          alt={pokemon.name}
-          src={pokemon.sprites.front_default}
-          className="pokemon-img"
-        />
-      </div>
-      <div className="card-body">
-        <div className="card-top">
-          <h3>{pokemon.name}</h3>
-          <div># {pokemon.id} </div>
+      <Card> 
+        <div className="pokemon-img-container">
+          <img
+            alt={pokemon.name}
+            src={pokemon.sprites.front_default}
+            className="pokemon-img"
+          />
         </div>
-        <div className="card-bottom">
-          <div className="pokemon-type">
-            {pokemon.types.map((type, index) => {
-              return (
-                <div className="pokemon-type-item" key={index}>
-                  {type.type.name}
-                </div>
-              );
-            })}
+        <div className="card-body">
+          <div className="card-top">
+            <h3>{pokemon.name}</h3>
+            <div># {pokemon.id} </div>
           </div>
-          <svg
-            className="pokeball"
-            onClick={() => likeButtonPressed(pokemon.id)}
-          >
-            <use xlinkHref={like ? "#likedPokeball" : "#pokeball"}></use>
-          </svg>
+          <div className="card-bottom">
+            <div className="pokemon-type">
+              {pokemon.types.map((type, index) => {
+                return (
+                  <div className="pokemon-type-item" key={index}>
+                    {type.type.name}
+                  </div>
+                );
+              })}
+            </div>
+            <svg
+              className="pokeball"
+              onClick={toggleFavStatus}
+            >
+              <use xlinkHref={pokemonIsFav ? "#likedPokeball" : "#pokeball"}></use>
+            </svg>
+          </div>
         </div>
-      </div>
+      </Card>
     </div>
   );
 }
